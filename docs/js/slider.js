4bfa44fee1e3e6910b16070c3ae6e4f526381bd0
Pablo <pablopukha@gmail.com>
2022-03-06 14:11:13 +0200

    added mobile version

--- docs/js/slider.js
+++ docs/js/slider.js
@@ -1,1 +1,1 @@
-$(".reviews__slider").slick({infinite:!0,fade:!0,dots:!0,centerMode:!0,arrows:!1});
+$(".reviews__slider").slick({infinite:!0,fade:!0,dots:!0,centerMode:!0,arrows:!1,responsive:[{breakpoint:767,settings:{slidesToShow:.5,slidesToScroll:1,fade:!0}}]});
--- src/js/slider.js
+++ src/js/slider.js
@@ -4,4 +4,15 @@ $('.reviews__slider').slick({
     dots: true,
     centerMode: true,
     arrows: false,
+    
+    responsive: [
+        {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 0.5,
+              slidesToScroll: 1,
+              fade: true,
+            }
+          },
+    ]
 });
